537a5314854b7201e30cddb6ff5836487315d3ef
Aaron Mangel <aaron.mangel@banno.com>
2013-06-28 13:47:35 -0500

    Updated ui. Removed additional bin/gen files.

--- src/com/mangel/signatureexample/MainActivity.java
+++ src/com/mangel/signatureexample/MainActivity.java
@@ -3,13 +3,24 @@ package com.mangel.signatureexample;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    @Override
+    private SignaturePadView mSignatureView;
+	private TextView mText;
+	private ImageView mImage;
+
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        mSignatureView = (SignaturePadView) findViewById(R.id.signatureView1);
+        mText = (TextView) findViewById(R.id.textView1);
+        mImage = (ImageView) findViewById(R.id.imageView);
     }
 
 
@@ -20,4 +31,19 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch (item.getItemId()) {
+    	case R.id.action_clear:
+    		mSignatureView.clear();
+    		return true;
+    	case R.id.action_hasBeenMarked:
+    		mText.setText(mSignatureView.hasBeenMarked() ? "Marked" : "Not marked");
+    		return true;
+    	case R.id.action_getImage:
+    		mImage.setImageBitmap(mSignatureView.getImage());
+    		return true;
+    	}
+    	return super.onOptionsItemSelected(item);
+    }
 }
